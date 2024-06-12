a193bdc9bc5defcc767e9c30ebaacf6dda7dcb98
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-06-11 22:29:52 -0300

    Adicionando icone no botão com o Font Awesome

--- frontend/src/components/create-employee/CreateEmployeeComponente.vue
+++ frontend/src/components/create-employee/CreateEmployeeComponente.vue
@@ -49,14 +49,8 @@ function handleSubmitForm() {
           <!-- INÍCIO BLOCO: Employee Name -->
           <div class="form-group">
             <label for="" class="font-weight-bold">Employee Name</label>
-            <input 
-              type="text" 
-              id="name" 
-              class="form-control" 
-              placeholder="Type Your Name" 
-              v-model="employee.name"
-              :class="{'is-invalid': v$.name.$error}"
-            >
+            <input type="text" id="name" class="form-control" placeholder="Type Your Name" v-model="employee.name"
+              :class="{'is-invalid': v$.name.$error}">
             <span v-if="v$.name.$error" class="is-invalid-span">
               The name field is mandatory and must have at least 2 characters
             </span>
@@ -65,15 +59,10 @@ function handleSubmitForm() {
 
           <!-- INÍCIO BLOCO: Job Role -->
           <div class="form-group">
-              <label for="" class="font-weight-bold">Job Role</label>
-              <input 
-                type="text" 
-                class="form-control" 
-                placeholder="Type Job" 
-                v-model="employee.job_role"
-                :class="{'is-invalid': v$.job_role.$error}"
-              >
-              <span v-if="v$.job_role.$error" class="is-invalid-span">
+            <label for="" class="font-weight-bold">Job Role</label>
+            <input type="text" class="form-control" placeholder="Type Job" v-model="employee.job_role"
+              :class="{'is-invalid': v$.job_role.$error}">
+            <span v-if="v$.job_role.$error" class="is-invalid-span">
               Fill in the field
             </span>
           </div>
@@ -82,14 +71,8 @@ function handleSubmitForm() {
           <!-- INÍCIO BLOCO: Salary-->
           <div class="form-group">
             <label for="" class="font-weight-bold">Salary</label>
-            <input 
-              type="number" 
-              id="salary" 
-              class="form-control" 
-              placeholder="Salary" 
-              v-model="employee.salary"
-              :class="{'is-invalid': v$.salary.$error}"
-            >
+            <input type="number" id="salary" class="form-control" placeholder="Salary" v-model="employee.salary"
+              :class="{'is-invalid': v$.salary.$error}">
             <span v-if="v$.salary.$error" class="is-invalid-span">
               The salary field is mandatory
             </span>
@@ -99,13 +82,8 @@ function handleSubmitForm() {
           <!-- INÍCIO BLOCO: Birth-->
           <div class="form-group">
             <label for="" class="font-weight-bold">Birth</label>
-            <input 
-              type="date" 
-              class="form-control" 
-              placeholder="YYYY/MM/DD" 
-              v-model="employee.birth"
-              :class="{'is-invalid': v$.birth.$error}"
-            >
+            <input type="date" class="form-control" placeholder="YYYY/MM/DD" v-model="employee.birth"
+              :class="{'is-invalid': v$.birth.$error}">
             <span v-if="v$.birth.$error" class="is-invalid-span">
               Fill in the field
             </span>
@@ -115,13 +93,8 @@ function handleSubmitForm() {
           <!-- INÍCIO BLOCO: Employee Registration-->
           <div class="form-group">
             <label for="" class="font-weight-bold">Employee Registration</label>
-            <input 
-              type="text" 
-              class="form-control" 
-              placeholder="Employee Registration"
-              v-model="employee.employee_registration"
-              :class="{'is-invalid': v$.employee_registration.$error}"
-            >
+            <input type="text" class="form-control" placeholder="Employee Registration"
+              v-model="employee.employee_registration" :class="{'is-invalid': v$.employee_registration.$error}">
             <span v-if="v$.employee_registration.$error" class="is-invalid-span">
               Fill in the field
             </span>
@@ -129,8 +102,11 @@ function handleSubmitForm() {
           <!-- FIM BLOCO: Employee Registration -->
 
           <div class="form-group">
-            <button class="btn btn-primary" type="submit">
-              + Employee
+            <button class="btn btn-primary">
+              <font-awesome-icon :icon="['fas', 'user-plus']" />
+              <!-- <font-awesome-icon :icon="['far', 'address-book']" /> -->
+              <!-- <font-awesome-icon :icon="['fab', 'github']" /> -->
+              Employee
             </button>
           </div>
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -12,13 +12,24 @@ import App from './App.vue';
 import router from './router';
 
 // Importações do Vualidate
-import Vuelidate from '@vuelidate/core'
+import Vuelidate from '@vuelidate/core';
 
-// Configuração do Vualidate
+// Importar Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { fas  } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+// Adicionar todos os ícones à biblioteca
+library.add(fas, far, fab);
 
 
 ///// Criação do app Vue////////
 const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Vualidate
 app.use(Vuelidate);
